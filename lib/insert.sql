408c0fae55fd1daa87da154bc2fd0bfe7b14364f
Nicholas Orochena <nicholas.orochena@gmail.com>
2019-05-19 22:30:39 -0400

    completed lab

---
+++ lib/insert.sql
@@ -0,0 +1,63 @@
+INSERT INTO users(name, age) VALUES ("Nick0", 25);
+INSERT INTO users(name, age) VALUES ("Nick1", 25);
+INSERT INTO users(name, age) VALUES ("Nick2", 25);
+INSERT INTO users(name, age) VALUES ("Nick3", 25);
+INSERT INTO users(name, age) VALUES ("Nick4", 25);
+INSERT INTO users(name, age) VALUES ("Nick5", 25);
+INSERT INTO users(name, age) VALUES ("Nick6", 25);
+INSERT INTO users(name, age) VALUES ("Nick7", 25);
+INSERT INTO users(name, age) VALUES ("Nick8", 25);
+INSERT INTO users(name, age) VALUES ("Nick9", 25);
+INSERT INTO users(name, age) VALUES ("Nick10", 25);
+INSERT INTO users(name, age) VALUES ("Nick11", 25);
+INSERT INTO users(name, age) VALUES ("Nick12", 25);
+INSERT INTO users(name, age) VALUES ("Nick13", 25);
+INSERT INTO users(name, age) VALUES ("Nick14", 25);
+INSERT INTO users(name, age) VALUES ("Nick15", 25);
+INSERT INTO users(name, age) VALUES ("Nick16", 25);
+INSERT INTO users(name, age) VALUES ("Nick17", 25);
+INSERT INTO users(name, age) VALUES ("Nick18", 25);
+INSERT INTO users(name, age) VALUES ("Nick19", 25);
+
+INSERT INTO projects(title, category, funding_goal, start_date, end_date) VALUES ("Nick Project", 20,000, "2019-05-19", "2019-08-23");
+INSERT INTO projects(title, category, funding_goal, start_date, end_date) VALUES ("Nick Project", 20,000, "2019-05-19", "2019-08-23");
+INSERT INTO projects(title, category, funding_goal, start_date, end_date) VALUES ("Nick Project", 20,000, "2019-05-19", "2019-08-23");
+INSERT INTO projects(title, category, funding_goal, start_date, end_date) VALUES ("Nick Project", 20,000, "2019-05-19", "2019-08-23");
+INSERT INTO projects(title, category, funding_goal, start_date, end_date) VALUES ("Nick Project", 20,000, "2019-05-19", "2019-08-23");
+INSERT INTO projects(title, category, funding_goal, start_date, end_date) VALUES ("Nick Project", 20,000, "2019-05-19", "2019-08-23");
+INSERT INTO projects(title, category, funding_goal, start_date, end_date) VALUES ("Nick Project", 20,000, "2019-05-19", "2019-08-23");
+INSERT INTO projects(title, category, funding_goal, start_date, end_date) VALUES ("Nick Project", 20,000, "2019-05-19", "2019-08-23");
+INSERT INTO projects(title, category, funding_goal, start_date, end_date) VALUES ("Nick Project", 20,000, "2019-05-19", "2019-08-23");
+INSERT INTO projects(title, category, funding_goal, start_date, end_date) VALUES ("Nick Project", 20,000, "2019-05-19", "2019-08-23");
+
+INSERT INTO pledges (id, amount, user_id, project_id) VALUES
+(1, 10.00, 1, 2),
+(2, 20.00, 1, 3),
+(3, 40.00, 1, 4),
+(4, 50.00, 2, 3),
+(5, 10.00, 3, 2),
+(6, 20.00, 4, 4),
+(7, 40.00, 5, 10),
+(8, 60.00, 6, 10),
+(9, 50.00, 7, 9),
+(10, 700.00, 8, 8),
+(11, 1000.00, 8, 7),
+(12, 40.00, 9, 6),
+(13, 50.00, 9, 3),
+(14, 50.00, 10, 4),
+(15, 24.00, 12, 1),
+(16, 34.00, 11, 1),
+(17, 12.00, 13, 6),
+(18, 19.00, 14, 5),
+(19, 20.00, 15, 5),
+(20, 40.00, 16, 6),
+(21, 35.50, 17, 7),
+(22, 40.00, 18, 8),
+(23, 60.00, 19, 9),
+(24, 70.00, 20, 10),
+(25, 100.00, 20, 4),
+(26, 40.00, 19, 1),
+(27, 20.00, 18, 6),
+(28, 90.00, 17, 9),
+(29, 230.00, 16, 6),
+(30, 450.00, 15, 5);
